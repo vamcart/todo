c88c1c70d2613bec919eb0f177c2c7f1191b2da1
vam <vamshop@gmail.com>
2023-06-06 21:28:14 +0300

    Удалён неиспользуемый код в модели user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,47 +6,4 @@ use App\Core\Model;
 
 class Todo extends Model
 {
-    /**
-     * @var string table associated with this model
-     */
-    protected $table = 'user';
-
-    /**
-     * @var array list of safe properties
-     */
-    protected $fillable = ['name', 'email', 'description', 'status'];
-
-    /**
-     * Get all records from db
-     * @return array An array of Todo
-     */
-    public static function all($sort, $order, $page, $limit)
-    {
-        $db = resolve('db');
-        $db->prepare('select * from todos order by ' . $sort . ' ' . $order . $limit);
-        $db->bindValue(':ttt', $sort);
-        $db->bindValue(':sort', $sort); 
-
-        $db->execute();
-        $result = $db->fetchAllAssociative();
-        $modelList = [];
-        // transform to array of models
-        foreach ($result as $data) {
-            $model = new Todo();
-            $model->_fill($data);
-            $modelList[] = $model;
-        }
-
-        return $modelList;
-    }
-    
-    public static function count_all()
-    {
-    	$db = resolve('db');
-    	$db->prepare("select * from todos");
-    	$db->execute();
-    	$count = $db->countRows();
-
-        return $count;
-    }    
 }
