24ca02b6bcd9abe229dd3f6b062242e98f5e3943
vam <vamshop@gmail.com>
2023-06-06 21:23:28 +0300

    Добавлена папка cache

--- config/app.php
+++ config/app.php
@@ -8,8 +8,8 @@ return [
         'host' => 'localhost',
         'port' => '3306',
         'user' => 'root',
-        'password' => '',
-        'db' => 'todo',
+        'password' => 'Rfetigb4s;',
+        'db' => 'todo2',
     ],
     // use short_url (e.g http://domain.com/some-route will become /some-route)
     'short_url' => true
